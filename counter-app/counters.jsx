1859e4779c023392a51402d126edc5d7545a4448
Prabhash <Prabhash Mohanty@Dell>
2018-10-21 19:04:35 +0530

    Ädded code for counter app react till reset button

---
+++ counter-app/counters.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Counter from "./counterComponents";
+
+class Counters extends Component {
+    state = { 
+        counters:[
+            {id:1,value:4},
+            {id:2,value:0},
+            {id:3,value:0},
+            {id:4,value:0},
+        ]
+     }
+
+     handleIncreement=(counter)=>
+     {
+        const counters=[...this.state.counters];
+        const index=counters.indexOf(counter);
+        counters[index]={...counter};
+        counters[index].value++;
+        this.setState({counters:counters});
+     }
+
+     handleReset=()=>
+     {
+        const counters=this.state.counters.map(c=>{
+            c.value=0;
+            return c;
+        });
+        this.setState({counters});
+     }
+
+     handleDelete=(counterId)=>
+     {
+         const counters=this.state.counters.filter(c=>c.id!==counterId);
+         this.setState({counters:counters});
+     };
+    render() { 
+        return (<div>
+            <button onClick={this.handleReset}
+            className="button-primary btn-sm btn-m2">Reset</button>
+        {this.state.counters.map(
+            counter=><Counter 
+            key={counter.id} 
+            onDelete={this.handleDelete} 
+            onIncreement={this.handleIncreement}
+            counter={counter}/>)}
+        </div>);
+    }
+}
+ 
+export default Counters;
